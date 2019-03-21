01ba749e8c011868dbe226c50add46731c55015b
philipp <ph.soff@gmail.com>
2019-03-21 13:26:10 +0200

    nodemon included

--- lesson_01/index/index.js
+++ lesson_01/index/index.js
@@ -1,5 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT;
+const ora = require('ora');
 
 if (NODE_ENV === 'red') {
   const joinArguments = require('./hw_task_3/module_01.js');
