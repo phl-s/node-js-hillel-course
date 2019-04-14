f5166bf09fdba2e4a3c57ecf979e225e32c21c70
philipp-sapronov <philipp.sapronov@gmail.com>
2019-04-14 10:53:28 +0300

    checking out

--- lesson_03/index/index.js
+++ lesson_03/index/index.js
@@ -1,3 +1,5 @@
+//Checkout
+
 const { i: inputPath, o: outputPath, d: depth } = require('minimist')(process.argv.slice(2));
 const pathNormalise = require('./pathNormalise');
 const parserInit = require('./directoriesParser');
